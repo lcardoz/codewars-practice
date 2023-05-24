fcd29af7979f59414ec129f09d97f0020e68a44b
Leah Cardoz <leahcardoz@gmail.com>
2023-05-24 11:20:50 -0400

    completed problem 4 who likes it? - javascript

--- index.js
+++ index.js
@@ -35,3 +35,27 @@ function rentalCarCost(d) {
   } else
     return (d * 40)
 }
+
+    // cleaner solution 1:
+    const rentalCarCost = d => d * 40 - ((d > 6) ? 50 : ((d > 2) ? 20 : 0));
+
+    // cleaner solution 2:
+    function rentalCarCost(d) {
+      return d * 40 - (d >= 7 ? 50 : (d >= 3 ? 20 : 0));
+    }
+
+// 4. Who likes it?
+
+function likes(names) {
+  if (names.length === 0) {
+    return "no one likes this"
+  } else if (names.length === 1) {
+    return `${names} likes this`
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  } else if (names.length >= 4) {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+  }
+}
