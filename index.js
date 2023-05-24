751e69b06dfedce369d3b0b1487cdaf622ef8668
Leah Cardoz <leahcardoz@gmail.com>
2023-05-24 10:49:04 -0400

    completed second problem- split strings

--- index.js
+++ index.js
@@ -3,4 +3,24 @@ function sayHello(name) {
   return 'Hello, ' + name 
 }
 
-// 2. 
+    // cleaner solution:
+    const sayHello = name => `Hello, ${name}`;
+
+// 2. Split Strings
+
+function solution(str) {
+  
+  let pairs = [];
+  
+  for (let i = 0; i < str.length; i += 2) {
+    let pair = str.slice(i, i + 2);
+    
+    if (pair.length === 1) {
+      pair += '_';
+    }
+    
+    pairs.push(pair);
+  }
+  
+  return pairs;
+}
