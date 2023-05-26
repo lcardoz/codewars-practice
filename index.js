821a896245e38d29838987056384ec445189e98f
Leah Cardoz <leahcardoz@gmail.com>
2023-05-25 21:42:32 -0400

    completed problem 9 - Create Phone Number - javascript

--- index.js
+++ index.js
@@ -99,3 +99,24 @@ function sumTwoSmallestNumbers(numbers){
   numbers.sort((a,b) => a - b);
   return numbers[0] + numbers[1];
 };
+
+// 8. Create Phone Number
+
+function createPhoneNumber(numbers){
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
+    // alternate solution:
+
+    function createPhoneNumber(numbers){
+      let format = "(xxx) xxx-xxxx";
+      
+      for(let i = 0; i < numbers.length; i++)
+      {
+        format = format.replace('x', numbers[i]);
+      }
+      
+      return format;
+    }
+    
+    
